32abf24cc0b6df6747543131510c111bc3913b7e
Pieter Vandendriessche <pieter.vandendendriessche@delaware.pro>
2020-06-01 11:17:37 +0200

    Added pnpjs

--- src/extensions/sharePointMsTeamsLikesAndComments/SharePointMsTeamsLikesAndCommentsApplicationCustomizer.ts
+++ src/extensions/sharePointMsTeamsLikesAndComments/SharePointMsTeamsLikesAndCommentsApplicationCustomizer.ts
@@ -1,7 +1,7 @@
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
-  BaseApplicationCustomizer
+  BaseApplicationCustomizer, PlaceholderContent, PlaceholderName
 } from '@microsoft/sp-application-base';
 import { Dialog } from '@microsoft/sp-dialog';
 
@@ -15,25 +15,43 @@ const LOG_SOURCE: string = 'SharePointMsTeamsLikesAndCommentsApplicationCustomiz
  * You can define an interface to describe it.
  */
 export interface ISharePointMsTeamsLikesAndCommentsApplicationCustomizerProperties {
-  // This is an example; replace with your own property
-  testMessage: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class SharePointMsTeamsLikesAndCommentsApplicationCustomizer
   extends BaseApplicationCustomizer<ISharePointMsTeamsLikesAndCommentsApplicationCustomizerProperties> {
 
+  private _footerPlaceholder: PlaceholderContent;
+
   @override
   public onInit(): Promise<void> {
-    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    //When a placeholderProvider has changed fire the render method again.
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
-    }
-
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
-
+    //On navigate event start render
+    this.context.application.navigatedEvent.add(this, () => {
+      this.startReactRender();
+    });
     return Promise.resolve();
   }
+  startReactRender() {
+    throw new Error("Method not implemented.");
+  }
+
+  private _renderPlaceHolders() {
+    //Check if the placeholder is already set
+    if (!this._footerPlaceholder && this.context.placeholderProvider.placeholderNames.indexOf(PlaceholderName.Bottom) !== -1) {
+      this._footerPlaceholder = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Bottom, {
+        onDispose: this._onDispose
+      });
+
+    }
+
+  }
+
+  private _onDispose(): void {
+    console.log('[MSTeamsPlaceholder._onDispose] Disposed.');
+  }
+
+
 }
